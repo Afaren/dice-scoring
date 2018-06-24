d78c2a218698bdb2d11c0448dbd8be0993a040ef
Sundaisy <daisyzs1213@163.com>
2018-06-24 20:39:20 +0800

    定义rule

--- javascript/lib/DiceScoring.js
+++ javascript/lib/DiceScoring.js
@@ -1,47 +1,50 @@
 const _ = require('lodash');
 
+const rule = {
+    '1': {
+        threeRatio: 1000,
+        singleRatio: 100
+    },
+    '5': {
+        threeRatio: 500,
+        singleRatio: 50
+    },
+    'other': {
+        threeRatio: 100,
+        singleRatio: 0
+    }
+};
+
 function scoring(...args) {
-  const numberCountMap = args.reduce((acc, cur) => {
-    acc[cur] ? acc[cur]++ : acc[cur] = 1;
-    return acc;
-  }, {});
+    const numberCountMap = args.reduce((acc, cur) => {
+        acc[cur] ? acc[cur]++ : acc[cur] = 1;
+        return acc;
+    }, {});
 
-  const result = calculateScore(numberCountMap);
+    const result = calculateScore(numberCountMap);
 
-  return _.values(result)
-               .reduce((acc, cur) => acc + cur, 0);
+    return _.values(result)
+        .reduce((acc, cur) => acc + cur, 0);
 }
 
 
 function calculateScore(numberCountMap) {
-  const result = {};
-  _.entries(numberCountMap)
-   .forEach(([number, count]) => {
-     switch (number) {
-       case '1':
-         if (!(count < 3)) {
-           result[number] = parseInt(count / 3) * 1000 + count % 3 * 100;
-         } else {
-           result[number] = count * 100
-         }
-         break;
-       case '5':
-         if (!(count < 3)) {
-           result[number] = parseInt(count / 3) * 100 * number + count % 3 * 50
-         } else {
-           result[number] = count * 50;
-         }
-         break;
-       default:
-         if (!(count < 3)) {
-           result[number] = parseInt(count / 3) * 100 * number;
-         } else {
-           result[number] = 0;
-         }
-         break;
-     }
-   });
-  return result;
+    const result = {};
+    _.entries(numberCountMap)
+        .forEach(([number, count]) => {
+            result[number] = calculateRatio(number, count);
+        });
+    return result;
+}
+
+function calculateRatio(number, count) {
+    rule['other'].threeRatio = 100 * number;
+    let calculateRule = rule[number] || rule['other'];
+    if (!(count < 3)) {
+        return parseInt(count / 3) * calculateRule.threeRatio + count % 3 * calculateRule.singleRatio;
+    } else {
+        return count * calculateRule.singleRatio;
+    }
 }
 
 module.exports = scoring;
