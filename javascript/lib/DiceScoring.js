308bf667720b0dbaa736ee86eda238a62655abd9
Chongfa <cfachen@thoughtworks.com>
2018-06-24 19:39:18 +0800

    afaren - variable rename, what is key and value

--- javascript/lib/DiceScoring.js
+++ javascript/lib/DiceScoring.js
@@ -18,27 +18,27 @@ function scoring() {
 }
 
 
-function calculateScore(numObj) {
+function calculateScore(numberCountMap) {
   const result = {};
-  Object.keys(numObj).forEach(number => {
+  Object.keys(numberCountMap).forEach(number => {
     switch (number) {
       case '1':
-        if (!(numObj[number] < 3)) {
-          result[number] = parseInt(numObj[number] / 3) * 1000 + numObj[number] % 3 * 100;
+        if (!(numberCountMap[number] < 3)) {
+          result[number] = parseInt(numberCountMap[number] / 3) * 1000 + numberCountMap[number] % 3 * 100;
         } else {
-          result[number] = numObj[number] * 100
+          result[number] = numberCountMap[number] * 100
         }
         break;
       case '5':
-        if (!(numObj[number] < 3)) {
-          result[number] = parseInt(numObj[number] / 3) * 100 * number + numObj[number] % 3 * 50
+        if (!(numberCountMap[number] < 3)) {
+          result[number] = parseInt(numberCountMap[number] / 3) * 100 * number + numberCountMap[number] % 3 * 50
         } else {
-          result[number] = numObj[number] * 50;
+          result[number] = numberCountMap[number] * 50;
         }
         break;
       default:
-        if (!(numObj[number] < 3)) {
-          result[number] = parseInt(numObj[number] / 3) * 100 * number;
+        if (!(numberCountMap[number] < 3)) {
+          result[number] = parseInt(numberCountMap[number] / 3) * 100 * number;
         } else {
           result[number] = 0;
         }
