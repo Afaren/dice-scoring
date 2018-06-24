cb0e928b0bc1cd304a67ff7bb15ffd97f453cb07
Chongfa <cfachen@thoughtworks.com>
2018-06-24 20:38:49 +0800

    afaren - use lodash due to Object.entries has compability issue

--- javascript/lib/DiceScoring.js
+++ javascript/lib/DiceScoring.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 function scoring(...args) {
   const numberCountMap = args.reduce((acc, cur) => {
     acc[cur] ? acc[cur]++ : acc[cur] = 1;
@@ -13,32 +15,32 @@ function scoring(...args) {
 
 function calculateScore(numberCountMap) {
   const result = {};
-  Object.entries(numberCountMap)
-        .forEach(([number, count]) => {
-          switch (number) {
-            case '1':
-              if (!(count < 3)) {
-                result[number] = parseInt(count / 3) * 1000 + count % 3 * 100;
-              } else {
-                result[number] = count * 100
-              }
-              break;
-            case '5':
-              if (!(count < 3)) {
-                result[number] = parseInt(count / 3) * 100 * number + count % 3 * 50
-              } else {
-                result[number] = count * 50;
-              }
-              break;
-            default:
-              if (!(count < 3)) {
-                result[number] = parseInt(count / 3) * 100 * number;
-              } else {
-                result[number] = 0;
-              }
-              break;
-          }
-        });
+  _.entries(numberCountMap)
+   .forEach(([number, count]) => {
+     switch (number) {
+       case '1':
+         if (!(count < 3)) {
+           result[number] = parseInt(count / 3) * 1000 + count % 3 * 100;
+         } else {
+           result[number] = count * 100
+         }
+         break;
+       case '5':
+         if (!(count < 3)) {
+           result[number] = parseInt(count / 3) * 100 * number + count % 3 * 50
+         } else {
+           result[number] = count * 50;
+         }
+         break;
+       default:
+         if (!(count < 3)) {
+           result[number] = parseInt(count / 3) * 100 * number;
+         } else {
+           result[number] = 0;
+         }
+         break;
+     }
+   });
   return result;
 }
 
