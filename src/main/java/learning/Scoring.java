e3c88fa8a351dec4a99e0bbad7be5ed0becf0798
Chongfa <cfachen@thoughtworks.com>
2018-06-24 11:03:46 +0800

    refactor

--- src/main/java/learning/Scoring.java
+++ src/main/java/learning/Scoring.java
@@ -1,51 +1,47 @@
 package learning;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Scoring {
-    public static int score(Integer... dices) {
-        Map<Integer, List<Integer>> groups = Arrays.stream(dices)
-                                                   .collect(Collectors.groupingBy(e -> e));
-        return groups.entrySet()
+    public static long score(Integer... dices) {
+        return Arrays.stream(dices)
+                     .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                     .entrySet()
                      .stream()
-                     .mapToInt(x -> calculate(x.getValue().stream().toArray(Integer[]::new)))
+                     .mapToLong(x -> calculate(x.getKey(), x.getValue()))
                      .sum();
     }
 
-    private static int calculate(Integer... dices) {
-        if (dices.length == 0) {
-            return 0;
+    private static long calculate(Integer number, Long count) {
+        if (number == 1) {
+            return one(count);
         }
-        if (dices[0] == 1) {
-            return one(dices.length);
+        if (number == 5) {
+            return five(count);
         }
-        if (dices[0] == 5) {
-            return five(dices.length);
+        if (number == 2 || number == 3 || number == 4 || number == 6) {
+            return other(number, count);
         }
-        if (dices[0] == 2 || dices[0] == 3 || dices[0] == 4 || dices[0] == 6) {
-            return other(dices[0], dices.length);
-        }
-        return 0;
+        return 0L;
     }
 
-    private static int other(Integer number, int length) {
+    private static long other(Integer number, long length) {
         if (length < 3) {
-            return 0;
+            return 0L;
         }
-        return number * 100 + other(number, length -3);
+        return number * 100 + other(number, length - 3);
     }
 
-    private static int five(int length) {
+    private static long five(long length) {
         if (length < 3) {
             return 50 * length;
         }
         return 500 + five(length - 3);
     }
 
-    private static int one(int length) {
+    private static long one(long length) {
         if (length < 3) {
             return 100 * length;
         }
