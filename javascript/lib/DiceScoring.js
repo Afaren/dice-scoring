51b033ba03a51712474e928125810637a3ee4aa0
Chongfa <cfachen@thoughtworks.com>
2018-06-24 19:52:03 +0800

    afaren - use entries instead of keys

--- javascript/lib/DiceScoring.js
+++ javascript/lib/DiceScoring.js
@@ -20,32 +20,32 @@ function scoring() {
 
 function calculateScore(numberCountMap) {
   const result = {};
-  Object.keys(numberCountMap).forEach(number => {
-    let count = numberCountMap[number];
-    switch (number) {
-      case '1':
-        if (!(count < 3)) {
-          result[number] = parseInt(count / 3) * 1000 + count % 3 * 100;
-        } else {
-          result[number] = count * 100
-        }
-        break;
-      case '5':
-        if (!(count < 3)) {
-          result[number] = parseInt(count / 3) * 100 * number + count % 3 * 50
-        } else {
-          result[number] = count * 50;
-        }
-        break;
-      default:
-        if (!(count < 3)) {
-          result[number] = parseInt(count / 3) * 100 * number;
-        } else {
-          result[number] = 0;
-        }
-        break;
-    }
-  });
+  Object.entries(numberCountMap)
+        .forEach(([number, count]) => {
+          switch (number) {
+            case '1':
+              if (!(count < 3)) {
+                result[number] = parseInt(count / 3) * 1000 + count % 3 * 100;
+              } else {
+                result[number] = count * 100
+              }
+              break;
+            case '5':
+              if (!(count < 3)) {
+                result[number] = parseInt(count / 3) * 100 * number + count % 3 * 50
+              } else {
+                result[number] = count * 50;
+              }
+              break;
+            default:
+              if (!(count < 3)) {
+                result[number] = parseInt(count / 3) * 100 * number;
+              } else {
+                result[number] = 0;
+              }
+              break;
+          }
+        });
   return result;
 }
 
